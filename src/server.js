a0a72e1eb16e388ea222b75c16f6143199ec837d
Thiago Torres Costa <thiago.t0rres1@hotmail.com>
2023-10-31 08:38:18 -0300

    integration with frontend

--- src/server.js
+++ src/server.js
@@ -4,9 +4,11 @@ require("dotenv/config")
 const express = require("express")
 const routes = require("./routes")
 const AppError = require("./utils/appError")
+const cors = require("cors")
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 app.use((error, request, response, next) => {
